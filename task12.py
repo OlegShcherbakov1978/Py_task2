5ae91909078753f1dec9680222df9529cde59682
Oleg <olmag@inbox.ru>
2023-03-13 13:54:12 +0300

    task 10, 12, 14 added

---
+++ task12.py
@@ -0,0 +1,33 @@
+a = int(input('введите сумму '))
+b = int(input('введите произведение '))
+
+for i in range(a):
+    for j in range(b):
+        if (i+j == a) and (i*j == b):
+            num1 = i
+            num2 = j
+
+
+print(f'First number: {num1}, second number: {num2}')
+
+
+# import math
+
+# print("Введите коэффициенты для уравнения")
+# print("ax^2 + bx + c = 0:")
+# a = float(input("a = "))
+# b = float(input("b = "))
+# c = float(input("c = "))
+
+# discr = b ** 2 - 4 * a * c
+# print("Дискриминант D = %.2f" % discr)
+
+# if discr > 0:
+#     x1 = (-b + math.sqrt(discr)) / (2 * a)
+#     x2 = (-b - math.sqrt(discr)) / (2 * a)
+#     print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
+# elif discr == 0:
+#     x = -b / (2 * a)
+#     print("x = %.2f" % x)
+# else:
+#     print("Корней нет")
